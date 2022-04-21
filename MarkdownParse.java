a3a61ae3cfed336756a023362a0c3019524ad700
ddavevo <ddavepersona@gmail.com>
2022-04-21 12:41:22 -0700

    addition() commit for JUnit

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -5,6 +5,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
+import org.junit.Test;
+
 public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
