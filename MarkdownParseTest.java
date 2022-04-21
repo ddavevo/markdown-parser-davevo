528465d46b67dfc765ba2353bb34a687ab802e3e
ddavevo <ddavepersona@gmail.com>
2022-04-21 13:35:47 -0700

    s

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,30 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
 public class MarkdownParseTest {
-    
+
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testGetLinks() throws IOException {
+
+        Path fileName = Path.of("/Users/ddavepersona/Documents/GitHub/markdown-parser-davevo/test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> actualLinks = MarkdownParse.getLinks(content);
+
+        ArrayList<String> expectedLinks = new ArrayList<>();
+        expectedLinks.add("https://something.com");
+        expectedLinks.add("some-thing.html");
+
+        assertEquals(actualLinks, expectedLinks);
+
     }
 }
