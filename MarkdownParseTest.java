65f1b357da5444f04dae9d637e86da03f4dd3902
ddavevo <ddavepersona@gmail.com>
2022-04-21 13:50:02 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -20,9 +20,51 @@ public class MarkdownParseTest {
         String content = Files.readString(fileName);
         ArrayList<String> actualLinks = MarkdownParse.getLinks(content);
 
+        ArrayList<String> getLinksExpected = new ArrayList<>();
+        getLinksExpected.add("https://something.com");
+        getLinksExpected.add("some-thing.html");
+
+        assertEquals(actualLinks, getLinksExpected);
+
+    }
+
+    @Test
+    public void testParse1() throws IOException {
+
+        Path fileName = Path.of("/Users/ddavepersona/Documents/GitHub/markdown-parser-davevo/breaking-files.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> actualLinks = MarkdownParse.getLinks(content);
+
+        ArrayList<String> expectedLinks1 = new ArrayList<>();
+        expectedLinks1.add("(https://parents.caprisun.com");
+
+        assertEquals(actualLinks, expectedLinks1);
+
+    }
+
+    @Test
+    public void testParse2() throws IOException {
+
+        Path fileName = Path.of("/Users/ddavepersona/Documents/GitHub/markdown-parser-davevo/breaking2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> actualLinks = MarkdownParse.getLinks(content);
+
         ArrayList<String> expectedLinks = new ArrayList<>();
-        expectedLinks.add("https://something.com");
-        expectedLinks.add("some-thing.html");
+        expectedLinks.add("[https://shop.blackpinkmusic.com");
+        expectedLinks.add("www.google.com");
+
+        assertEquals(actualLinks, expectedLinks);
+
+    }
+    @Test
+    public void testParse3() throws IOException {
+
+        Path fileName = Path.of("/Users/ddavepersona/Documents/GitHub/markdown-parser-davevo/breaking3.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> actualLinks = MarkdownParse.getLinks(content);
+
+        ArrayList<String> expectedLinks = new ArrayList<>();
+        expectedLinks.add("https://shop.blackpinkmusic.com]");
 
         assertEquals(actualLinks, expectedLinks);
 
