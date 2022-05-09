6533940dde1aa8b6b5eae074f35126e321056682
ddavevo <ddavepersona@gmail.com>
2022-05-08 22:53:20 -0700

    Editing for Lab Report

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -44,5 +44,7 @@ public class MarkdownParse {
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
+
+        System.out.println("THINGY");
     }
 }
